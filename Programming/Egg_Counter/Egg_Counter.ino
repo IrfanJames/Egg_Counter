1a5ba196201794465fa8028d260f67f75dad8f3d
IrfanJames <irfanjamespak@gmail.com>
2023-02-25 17:27:18 +0500

    Final Code for Counting just One Lane

--- Programming/Egg_Counter/Egg_Counter.ino
+++ Programming/Egg_Counter/Egg_Counter.ino
@@ -1,10 +1,7 @@
-#define Deg_To_Rad 0.0174533f
-#define Rad_To_Deg 57.2958f
-
-const int IR_Sensor_Count = 3;
-int IR[IR_Sensor_Count] = {32, 34, 35};
-const float one_EGG_Time_ms = 1500;
-unsigned long time_Up = 0;
+const int IR_Sensor_Count = 1; //3;
+int IR[IR_Sensor_Count] = {32}; // 34, 35};
+const unsigned long one_EGG_Time_ms = 6000;
+unsigned long time_Up = millis();
 int egg_Count = 0;
 
 bool Sense();
@@ -20,25 +17,31 @@ void setup() {
 }
 
 void loop() {
-  Serial.printf("Egg Count: %d\t", egg_Count);
+  Serial.printf("\nEgg Count: %d\t", egg_Count);
+  Serial.print(millis());
+  Serial.print(" - ");
+  Serial.print(time_Up);
+  Serial.print(" = ");
+  Serial.print(millis() - time_Up);
   
   if (Sense() == false) {
     if (edge == true) {
       Count();
       Serial.print("\tObject Passed");
     }
-
+    else
+      Serial.print("\tNo Object Detected");
     edge = false;
-    Serial.print("\tNo Object Detected");
   }
   else {
     if (edge == false) {
       time_Up = millis();
       Serial.print("\tObject Detected");
     }
-
+    else    
+      Serial.print("\tObject Passing");
+    
     edge = true;
-    Serial.print("\tObject Passing");
   }
 }
 
@@ -48,26 +51,36 @@ bool Sense() {
   // Returns 1 if even one of the sensors deect an object
 
   for (int i = 0; i < IR_Sensor_Count; i++)
-    if (analogRead(IR[i] == HIGH)) return 1;
+    if (digitalRead(IR[i]) == LOW)
+      return 1;
   
   return 0;
 }
 
 bool Count() {
-  // This Functions gets evoked no object is detected
+  // This Functions gets evoked when no object is detected
   // If the past HIGH-Time (time when sensors constantly detected an object) is
   // long enough (~one_EGG_Time_ms) the egg_Count increments
 
   // If the past HIGH-Time (time when sensors constantly detected an object) is
   // twice the (~one_EGG_Time_ms * k) the egg_Count incremented by k
   
-  float margin = 0.3; // 0.1 == 10%
-  float k = (time_Up - one_EGG_Time_ms) / one_EGG_Time_ms;
+  unsigned long time = millis() - time_Up;
+  Serial.print("\t In Count: ");
+  Serial.print(time);
+
+  float margin = 0.1; // 0.1 == 10%
+  float k = (double)time / (double)one_EGG_Time_ms;
   
-  if (k < abs(1 - margin))
+  Serial.print("\t, k: ");
+  Serial.print(k);
+  Serial.print(", int(k): ");
+  Serial.print(int(k));
+  
+  if (k < abs(1.0f - margin))
     return 0;
   else {
-    egg_Count =+ (int)k; // (int)k = no 0f eggs passed
+    egg_Count += (int)abs(k); // (int)k = no 0f eggs passed
     return 1;
   }
 }
